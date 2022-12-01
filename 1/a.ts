ac26615f7b83af08761c5c376bba9e8adc8d7b3b
nicofishman <nicourifishman@gmail.com>
2022-12-01 10:20:30 -0300

    Day 1 cleanup

--- 1/a.ts
+++ 1/a.ts
@@ -1,7 +1,7 @@
 const text = await Deno.readTextFile("./input.txt");
 
 // split text into arrays separated by a blank line
-const groups = text.split("\n").map((each) => each.replace('\n', ''));
+const groups = text.split("\r").map((each) => each.replace('\n', ''));
 
 const elves: number[][] = [[]]
 
@@ -13,18 +13,12 @@ for (let i = 0; i < groups.length; i++) {
         elves[elves.length - 1].push(parseInt(groups[i]))
     }
 }
-const sortedElves = elves.sort((a, b) => b.reduce((a1, b1) => a1 + b1, 0) - a.reduce((a2, b2) => a2 + b2, 0))
 
-let max = 0;
-for (let i = 0; i < elves.length; i++) {
-    const sum = elves[i].reduce((a, b) => a + b, 0)
-    if (sum > max) {
-        max = sum
-    }
-}
+const sortedElves = elves.sort((a, b) => b.reduce((a1, b1) => a1 + b1, 0) - a.reduce((a2, b2) => a2 + b2, 0))
+console.log('PART 1:', sortedElves[0].reduce((a, b) => a + b, 0));
 
 const sum = sortedElves.splice(0, 3).reduce((a, b) => a + b.reduce((a1, b1) => a1 + b1, 0), 0)
-console.log(sum);
+console.log('PART 2:', sum);
 
 
 
